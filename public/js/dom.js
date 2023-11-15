231536e4b358de4ed15154014d259a059ca4e2ed
verakunca <vkunca@pioix.edu.ar>
2023-11-15 15:07:14 -0300

    JUEGO GEO TERMINADOOOO

--- public/js/dom.js
+++ public/js/dom.js
@@ -59,6 +59,458 @@ function irALogin() {
   location.href = '/register'
 }
 
+//juego geo
+ // VARIABLE DECLARATIONS ------
+  
+      // pages
+      var initPage,
+      questionsPage,
+      resultsPage,
+      // buttons
+      startBtn,
+      submitBtn,
+      continueBtn,
+      retakeBtn,
+      spanishBtn,
+      // question and answers
+      question,
+      answerList,
+      answerSpan,
+      answerA,
+      answerB,
+      answerC,
+      answerD,
+      // event listeners
+      answerDiv,
+      answerDivA,
+      answerDivB,
+      answerDivC,
+      answerDivD,
+      feedbackDiv,
+      selectionDiv,
+      toBeHighlighted,
+      toBeMarked,
+      userScore,
+      // quiz
+      quiz,
+      questionCounter,
+      correctAnswer,
+      correctAnswersCounter,
+      userSelectedAnswer,
+      // function names
+      newQuiz,
+      generateQuestionAndAnswers,
+      getCorrectAnswer,
+      getUserAnswer,
+      selectAnswer,
+      deselectAnswer,
+      selectCorrectAnswer,
+      deselectCorrectAnswer,
+      getSelectedAnswerDivs,
+      highlightCorrectAnswerGreen,
+      highlightIncorrectAnswerRed,
+      clearHighlightsAndFeedback;
+
+//funciones
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+$(document).ready(function(){
+ 
+  // DOM SELECTION ------
+  
+    // App pages
+      // Page 1 - Initial
+    initPage = $('.init-page');
+      // Page 2 - Questions/answers
+    questionsPage = $('.questions-page');
+      // Page 3 - Results
+    resultsPage = $('.results-page');
+  
+    // Buttons
+    startBtn = $('.init-page__btn');
+    submitBtn = $('.questions-page__submit-btn');
+    continueBtn = $('.questions-page__continue-btn');
+    retakeBtn = $('.results-page__retake-btn');
+    spanishBtn = $('.results-page__spanish-btn');
+  
+    // Answer block divs
+    answerDiv = $('.questions-page__answer-div');
+    answerDivA = $('.questions-page__answer-div-a');
+    answerDivB = $('.questions-page__answer-div-b');
+    answerDivC = $('.questions-page__answer-div-c');
+    answerDivD = $('.questions-page__answer-div-d');
+  
+    // Selection div (for the pointer, on the left)
+    selectionDiv = $('.questions-page__selection-div');
+  
+    // Feedback div (for the checkmark or X, on the right)
+    feedbackDiv = $('.questions-page__feedback-div');
+  
+    // Questions and answers
+    question = $('.questions-page__question');
+    answerList = $('.questions-page__answer-list');
+    answerSpan = $('.questions-page__answer-span');
+    answerA = $('.questions-page__answer-A');
+    answerB = $('.questions-page__answer-B');
+    answerC = $('.questions-page__answer-C');
+    answerD = $('.questions-page__answer-D');
+  
+    // User final score
+    userScore = $('.results-page__score');
+  
+  // QUIZ (PODEMOS AGREGAR TODAS LAS BANDERAS DEL MUNDO) ------
+    quiz = [
+     {
+    question: "¿De qué país es esta bandera?",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Flag_of_Brazil.svg/300px-Flag_of_Brazil.svg.png",
+    answers: ["Argentina", "México", "Brasil", "Chile"],
+    correctAnswer: "Brasil"
+  },
+  {
+    question: "¿De qué país es esta bandera?",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Flag_of_France_%281794%E2%80%931815%2C_1830%E2%80%931974%29.svg/200px-Flag_of_France_%281794%E2%80%931815%2C_1830%E2%80%931974%29.svg.png",
+    answers: ["España", "Francia", "Italia", "Alemania"],
+    correctAnswer: "Francia"
+  },
+  {
+    question: "¿De qué país es esta bandera?",
+    imageUrl: "https://s1.significados.com/foto/bandera-japon_sm.png", 
+    answers: ["China", "Corea del Sur", "Japón", "India"],
+    correctAnswer: "Japón"
+  },
+  {
+    question: "¿De qué país es esta bandera?",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Flag_of_Germany.svg/langes-1500px-Flag_of_Germany.svg.png", 
+    answers: ["Francia", "Italia", "Alemania", "España"],
+    correctAnswer: "Alemania"
+  },
+  {
+    question: "¿De qué país es esta bandera?",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Flag_of_Italy.svg/310px-Flag_of_Italy.svg.png",
+    answers: ["España", "México", "Italia", "Argentina"],
+    correctAnswer: "Italia"
+  },
+  {
+    question: "¿De qué país es esta bandera?",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Flag_of_Canada.svg/2560px-Flag_of_Canada.svg.png",
+    answers: ["Estados Unidos", "Canadá", "Reino Unido", "Australia"],
+    correctAnswer: "Canadá"
+  },
+  {
+    question: "¿De qué país es esta bandera?",
+    imageUrl: "https://img.freepik.com/vector-gratis/bandera-china-estilo-dibujos-animados-aislado-sobre-fondo-blanco_1308-67445.jpg",
+    answers: ["India", "China", "Japón", "Corea del Sur"],
+    correctAnswer: "China"
+  },
+  {
+    question: "¿De qué país es esta bandera?",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Flag_of_Argentina.svg/1200px-Flag_of_Argentina.svg.png",
+    answers: ["México", "Chile", "Brasil", "Argentina"],
+    correctAnswer: "Argentina"
+  },
+  {
+    question: "¿De qué país es esta bandera?",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/8/88/Flag_of_Australia_%28converted%29.svg",
+    answers: ["Nueva Zelanda", "Canadá", "Australia", "Sudáfrica"],
+    correctAnswer: "Australia"
+  },
+  {
+    question: "¿De qué país es esta bandera?",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/f/f3/Flag_of_Russia.svg",
+    answers: ["China", "Rusia", "India", "Estados Unidos"],
+    correctAnswer: "Rusia"
+  },
+  {
+    question: "¿De qué país es esta bandera?",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Flag_of_South_Africa.svg/2560px-Flag_of_South_Africa.svg.png",
+    answers: ["Nigeria", "Sudáfrica", "Egipto", "Ghana"],
+    correctAnswer: "Sudáfrica"
+  },
+  {
+    question: "¿De qué país es esta bandera ?",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Flag_of_Colombia.svg/200px-Flag_of_Colombia.svg.png",
+    answers: ["Perú", "Colombia", "Ecuador", "Venezuela"],
+    correctAnswer: "Colombia"
+  },
+  {
+    question: "¿De qué país es esta bandera?",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Flag_of_Chile.svg/2560px-Flag_of_Chile.svg.png",
+    answers: ["Argentina", "Perú", "Chile", "Bolivia"],
+    correctAnswer: "Chile"
+  },
+  {
+    question: "¿De qué país es esta bandera?",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Flag_of_Peru_%281825%E2%80%931884%29.svg/270px-Flag_of_Peru_%281825%E2%80%931884%29.svg.png",
+    answers: ["Chile", "Perú", "Ecuador", "Colombia"],
+    correctAnswer: "Perú"
+  },
+   {
+  question: "¿De qué país es esta bandera?",
+  imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Flag_of_Greece.svg/1200px-Flag_of_Greece.svg.png",
+  answers: ["Inglaterra", "Sudáfrica", "Islandia", "Grecia"],
+  correctAnswer: "Grecia"
+},
+{
+  question: "¿De qué país es esta bandera?",
+  imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Hopkinson_Flag.svg/200px-Hopkinson_Flag.svg.png",
+  answers: ["Mexico", "Estados Unidos", "El salvador", "Inglaterra"],
+  correctAnswer: "Estados Unidos"
+},
+{
+  question: "¿De qué país es esta bandera?",
+  imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Flag_of_Turkey.svg/1024px-Flag_of_Turkey.svg.png",
+  answers: ["Turquía", "Afganistan", "Turkmenistan", "Nigeria"],
+  correctAnswer: "Turquía"
+},
+{
+  question: "¿De qué país es esta bandera?",
+  imageUrl: "https://upload.wikimedia.org/wikipedia/commons/b/bd/Flag_of_Cuba.svg",
+  answers: ["Alemania", "Ecuador", "Cuba", "Iran"],
+  correctAnswer: "Cuba"
+},
+                  {
+  question: "¿De qué país es esta bandera?",
+  imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Flag_of_Israel.svg/1280px-Flag_of_Israel.svg.png",
+  answers: ["Grecia", "Israel", "Argentina", "Honduras"],
+  correctAnswer: "Israel"
+},
+
+];
+  
+// FUNCTION DECLARATIONS ------
+  
+  // Start the quiz
+  newQuiz = function() { 
+    // Set the question counter to 0
+    questionCounter = 0;
+    
+    // Set the total correct answers counter to 0
+    correctAnswersCounter = 0;
+    
+    // Hide other pages of the app
+    questionsPage.hide();
+    resultsPage.hide();
+  };
+  
+  // Load the next question and set of answers
+  generateQuestionAndAnswers = function() {
+  shuffleArray(quiz);
+    if (questionCounter % 5 === 0) {
+    shuffleArray(quiz);
+  }
+   if (questionCounter < 5) {
+    let image = document.getElementById("question-image");
+    question.text(quiz[questionCounter].question);
+    answerA.text(quiz[questionCounter].answers[0]);
+    answerB.text(quiz[questionCounter].answers[1]);
+    answerC.text(quiz[questionCounter].answers[2]);
+    answerD.text(quiz[questionCounter].answers[3]);
+    image.src = quiz[questionCounter].imageUrl;
+   } else {
+      questionsPage.hide();
+    resultsPage.show(300);
+    userScore.text(Math.floor((correctAnswersCounter / 5) * 100) + "%");
+  }
+};
+  
+  // Store the correct answer of a given question
+  getCorrectAnswer = function() {
+    correctAnswer = quiz[questionCounter].correctAnswer;
+  };
+  
+  // Store the user's selected (clicked) answer
+  getUserAnswer = function(target) {
+    userSelectedAnswer = $(target).find(answerSpan).text();
+  };
+
+  // Add the pointer to the clicked answer
+  selectAnswer = function(target) {
+    $(target).find(selectionDiv).addClass('ion-chevron-right');
+  };
+
+  // Remove the pointer from any answer that has it
+  deselectAnswer = function() {
+    if (selectionDiv.hasClass('ion-chevron-right')) {
+      selectionDiv.removeClass('ion-chevron-right');
+    }
+  };
+  
+  // Get the selected answer's div for highlighting purposes
+  getSelectedAnswerDivs = function(target) {
+    toBeHighlighted = $(target);
+    toBeMarked = $(target).find(feedbackDiv);
+  };
+  
+  // Make the correct answer green and add checkmark
+  highlightCorrectAnswerGreen = function(target) {
+    if (correctAnswer === answerA.text()) {
+      answerDivA.addClass('questions-page--correct');
+      answerDivA.find(feedbackDiv).addClass('ion-checkmark-round');
+    }
+    if (correctAnswer === answerB.text()) {
+      answerDivB.addClass('questions-page--correct');
+      answerDivB.find(feedbackDiv).addClass('ion-checkmark-round');
+    }
+    if (correctAnswer === answerC.text()) {
+      answerDivC.addClass('questions-page--correct');
+      answerDivC.find(feedbackDiv).addClass('ion-checkmark-round');
+    }
+    if (correctAnswer === answerD.text()) {
+      answerDivD.addClass('questions-page--correct');
+      answerDivD.find(feedbackDiv).addClass('ion-checkmark-round');
+    }
+  };
+  
+  // Make the incorrect answer red and add X
+  highlightIncorrectAnswerRed = function() {
+    toBeHighlighted.addClass('questions-page--incorrect');
+    toBeMarked.addClass('ion-close-round');
+  };
+  
+  // Clear all highlighting and feedback
+  clearHighlightsAndFeedback = function() {
+    answerDiv.removeClass('questions-page--correct');
+    answerDiv.removeClass('questions-page--incorrect');
+    feedbackDiv.removeClass('ion-checkmark-round');
+    feedbackDiv.removeClass('ion-close-round');
+  };
+  
+// APP FUNCTIONALITY ------
+  
+  /* --- PAGE 1/3 --- */
+  
+  // Start the quiz:
+  newQuiz();
+ // Clicking on start button:
+startBtn.on('click', function(){
+  
+  // Advance to questions page
+  initPage.hide();
+  questionsPage.show(300);
+  
+  // Load question and answers
+  generateQuestionAndAnswers();
+  
+  // Store the correct answer in a variable
+  getCorrectAnswer();
+  
+  // Hide the submit and continue buttons
+  submitBtn.hide();
+  continueBtn.hide();
+  
+});  
+ 
+  
+  /* --- PAGE 2/3 --- */
+  
+  // Clicking on an answer:
+  answerDiv.on('click', function(){
+    
+    // Make the submit button visible
+    submitBtn.show(300);
+    
+    // Remove pointer from any answer that already has it
+    deselectAnswer();
+    
+    // Put pointer on clicked answer
+    selectAnswer(this);
+    
+    // Store current selection as user answer
+    getUserAnswer(this);
+    
+    // Store current answer div for highlighting purposes
+    getSelectedAnswerDivs(this);
+    
+  });
+  
+  // Clicking on the submit button:
+  submitBtn.on('click', function(){
+    
+    // Disable ability to select an answer
+    answerDiv.off('click');
+      
+    // Make correct answer green and add a checkmark
+    highlightCorrectAnswerGreen();
+    
+    // Evaluate if the user got the answer right or wrong
+    if (userSelectedAnswer === correctAnswer) {
+      // Increment the total correct answers counter
+      correctAnswersCounter++;
+    }
+    else {
+      highlightIncorrectAnswerRed();
+    }
+    
+    // Substitute the submit button for the continue button:
+    submitBtn.hide(300);
+    continueBtn.show(300);
+    
+  });
+  
+  // Clicking on the continue button:
+  continueBtn.on('click', function(){
+    
+    // Increment question number until there are no more questions, then advance to the next page
+    if (questionCounter < quiz.length - 1) {
+      questionCounter++;
+    }
+    else {
+      questionsPage.hide();
+      resultsPage.show(300);
+      // Display user score as a percentage
+      userScore.text(Math.floor((correctAnswersCounter / quiz.length) * 100) + "%");
+    }
+    
+    // Load the next question and set of answers
+    generateQuestionAndAnswers();
+    
+    // Store the correct answer in a variable
+    getCorrectAnswer();
+    
+    // Remove all selections, highlighting, and feedback
+    deselectAnswer();
+    clearHighlightsAndFeedback();
+    
+    // Hide the continue button
+    continueBtn.hide(300);
+    
+    // Enable ability to select an answer
+    answerDiv.on('click', function(){
+      // Make the submit button visible
+      submitBtn.show(300);
+      // Remove pointer from any answer that already has it
+      deselectAnswer();
+      // Put pointer on clicked answer
+      selectAnswer(this);
+      // Store current answer div for highlighting purposes
+      getSelectedAnswerDivs(this);
+      // Store current selection as user answer
+      getUserAnswer(this);
+    });
+    
+  });
+  
+  /* --- PAGE 3/3 --- */
+
+  // Clicking on the retake button:
+  retakeBtn.on('click', function(){
+    
+    // Go to the first page
+    resultsPage.hide();
+    initPage.show(300);
+  
+    // Start the quiz over
+    newQuiz();
+    
+  });
+
+  
+});
 /*INGLES*/
 
 function delay(timeInMs) {
@@ -191,7 +643,7 @@ function checkRespuesta3(numero, elegido, correcto, numCorrecto) {
   }
   putJSON4(data4);
 }
-/*jueg matematica 
+//jueg matematica 
 const problemElement = document.querySelector(".problem")
 const ourForm = document.querySelector(".our-form")
 const ourField = document.querySelector(".our-field")
@@ -251,7 +703,7 @@ function handleSubmit(e) {
   }
   checkLogic()
 }
-/*chequea que el resultado sea siempre positivo
+//chequea que el resultado sea siempre positivo
 
 function generateProblem() {
   let numberOne, numberTwo, operator, correctAnswer;
@@ -309,7 +761,6 @@ function renderProgressBar() {
   progressBar.style.transform = `scaleX(${state.score / 10})`
 }
 
-*/
 
 //api
 
@@ -523,3 +974,4 @@ async function fetchAgregarPregunta(data) {
     console.error("Error:", error);
   }
 }
+
